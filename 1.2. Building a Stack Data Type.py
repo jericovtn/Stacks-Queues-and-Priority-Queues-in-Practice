8d13b681821473adfff400625f5a217e9ca52db4
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-12 23:37:14 +0800

    Added: heappush, stack, and print

--- 1.2. Building a Stack Data Type.py
+++ 1.2. Building a Stack Data Type.py
@@ -3,3 +3,4 @@ from queues import Stack
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
     print(element)
+
---
+++ 1.3. Representing Priority Queues With a Heap.py
@@ -0,0 +1,17 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 4: Stacks, Queues, and Priority Queues in Practice
+# 1. Implementing Queues in Python
+# November 19, 2022
+
+# 3. Representing Priority Queues With a Heap
+from heapq import heappush
+
+fruits = []
+
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+fruits
+
+

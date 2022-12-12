79dd8c7472256ef36d2cacf7df4fa2c02d9df36a
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-12 22:47:25 +0800

    Added: Informations

---
+++ queues.py
@@ -0,0 +1,25 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 4: Stacks, Queues, and Priority Queues in Practice
+# 1. Implementing Queues in Python
+# 1. Building a Queue Data Type
+# November 19, 2022
+
+# Queues: Class
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()

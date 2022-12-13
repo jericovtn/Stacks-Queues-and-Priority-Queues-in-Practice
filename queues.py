cd841d43b29f66a7447dd3b37b4220b81f7fba9d
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-13 18:55:45 +0800

    Added: Class for Mixin Class

--- queues.py
+++ queues.py
@@ -31,13 +31,13 @@ class Stack(Queue):
         return self._elements.pop()
 
 # 3. Representing Priority Queues With a Heap
-# Do not have Class
+# Does not have Class
 
 # 4. Building a Priority Queue Data Type
 
 from heapq import heappop, heappush
 
-# 5. Handling Corner Cases in Your Priority Queue=
+# 5. Handling Corner Cases in Your Priority Queue
 from itertools import count
 
 # Class for Priority Queue
@@ -59,3 +59,37 @@ class PriorityQueue:
         return heappop(self._elements)[-1]
 
 
+# 6. Refactoring the Code Using a Mixin Class
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+class PriorityQueue(IterableMixin):
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]

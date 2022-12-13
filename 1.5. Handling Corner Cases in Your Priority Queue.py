3774fe9e4d5080c5c211911b50f14d0d3aa936fa
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-13 19:01:32 +0800

    Added: Comment in File 1.5

--- 1.5. Handling Corner Cases in Your Priority Queue.py
+++ 1.5. Handling Corner Cases in Your Priority Queue.py
@@ -9,4 +9,7 @@ messages.enqueue_with_priority(CRITICAL, "wipers")
 messages.enqueue_with_priority(IMPORTANT, "hazard_lights")
 
 print(messages.dequeue())
+print(messages.dequeue())
 
+# This file still exist for formality purposes only
+# Codes in this file was merged in file 1.4

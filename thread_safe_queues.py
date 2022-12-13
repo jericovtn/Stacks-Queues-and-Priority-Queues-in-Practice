7e9bc8075d88425bb4c82d265118a5c87db3309a
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-13 22:30:23 +0800

    Define the entry point that will parse arguments

---
+++ thread_safe_queues.py
@@ -0,0 +1,31 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 4: Stacks, Queues, and Priority Queues in Practice
+# 3. Using Thread-Safe Queues
+# December 17, 2022
+
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
+
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    try:
+        main(parse_args())
+    except KeyboardInterrupt:
+        pass

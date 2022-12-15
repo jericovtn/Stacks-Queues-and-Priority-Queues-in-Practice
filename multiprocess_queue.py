522b4e5a3c3670da31452ea508a522fe3187d2a0
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-16 00:09:43 +0800

    Added poisonPill as the lastElement in input queue

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -109,6 +109,9 @@ def main(args):
         for indices in chunk_indices(len(combinations), len(workers)):
             queue_in.put(Job(combinations, *indices))
 
+    # Added poison pill as the last element in the input queue
+    queue_in.put(POISON_PILL)
+
     # Added
     while any(worker.is_alive() for worker in workers):
         try:

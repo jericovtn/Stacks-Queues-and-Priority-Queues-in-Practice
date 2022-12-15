237675d724cc404f48562fa7d204dec8f67ce84a
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-15 22:36:02 +0800

    Added fetching content and parsing HTML links

--- async_queues.py
+++ async_queues.py
@@ -11,10 +11,15 @@
 import argparse
 import asyncio
 from collections import Counter
-
 import aiohttp
 
-# Building Blocks
+# 2
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+
+
+# 1: Building Blocks
 async def main(args):
     session = aiohttp.ClientSession()
     try:
@@ -29,6 +34,19 @@ def parse_args():
     parser.add_argument("-d", "--max-depth", type=int, default=2)
     parser.add_argument("-w", "--num-workers", type=int, default=3)
     return parser.parse_args()
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+# 2: Added fetching content and parsing HTML links
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
 
 def display(links):
     for url, count in links.most_common():

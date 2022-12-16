14e43b9a0ca9a2e3248082d5c67956b6251bbfdf
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-16 13:18:57 +0800

    Created Producer for Apache Kafka

---
+++ consumer1.py
@@ -0,0 +1,12 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 4: Stacks, Queues, and Priority Queues in Practice
+# 6. Integrating Python With Distributed Message Queues
+# 3. Apache Kafka: kafka-python3
+# December 17, 2022
+
+from kafka3 import KafkaConsumer
+
+consumer = KafkaConsumer("datascience")
+for record in consumer:
+    message = record.value.decode("utf-8")
+    print(f"Got message: {message}")

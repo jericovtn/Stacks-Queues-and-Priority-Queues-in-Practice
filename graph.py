dbf42c33cc12518043dbb75e4303b4ce59c849dd
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2022-12-16 13:50:42 +0800

    Import networkx as nx

---
+++ graph.py
@@ -0,0 +1,8 @@
+# Name: Jerico James F. Viteño
+# Laboratory Exercise 4: Stacks, Queues, and Priority Queues in Practice
+# 2. Using Queues in Practice
+# 1. Object Representation of the Cities and Roads
+# December 17, 2022
+
+import networkx as nx
+print(nx.nx_agraph.read_dot("roadmap.dot"))
